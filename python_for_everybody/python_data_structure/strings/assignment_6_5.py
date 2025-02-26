d2ce3bcc868fe5de27e5fa35e88d79a66f1fd095
shy-dell <76672306+shy-dell@users.noreply.github.com>
2025-02-25 20:18:43 -0500

    fix: submit assignment 6.5

--- python_for_everybody/python_data_structure/strings/assignment_6_5.py
+++ python_for_everybody/python_data_structure/strings/assignment_6_5.py
@@ -1,1 +1,8 @@
-print(len('banana')* 7)
+text = "X-DSPAM-Confidence:    0.8475"
+
+stpos = text.find('0')
+
+num = text[stpos:].strip()
+#note: strip is unnecessary but probably good practice to ensure we don't have hidden whitespace
+
+print(float(num))
