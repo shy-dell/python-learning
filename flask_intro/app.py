670229c23efabfbdd412fb00ab493cfe904e1a40
shy-dell <76672306+shy-dell@users.noreply.github.com>
2025-03-02 17:47:45 -0500

    fix database columns

--- flask_intro/app.py
+++ flask_intro/app.py
@@ -4,12 +4,15 @@ from datetime import datetime, timezone
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
 # /// is a relative path, //// is an abs path for sqlite
+
+db = SQLAlchemy(app)
+
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False) #null false b/c don't want this blank
+    completed = db.Column(db.Integer, default=0)
     date_created = db.Column(db.DateTime, default=datetime.now(timezone.utc))
 
     def __repr__(self):
@@ -19,5 +22,5 @@ class Todo(db.Model):
 def index():
     return render_template('index.html')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
