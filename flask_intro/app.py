0b23e89f7a1054f431274fe78910f5ab0f911aaf
shy-dell <76672306+shy-dell@users.noreply.github.com>
2025-03-02 17:35:37 -0500

    create database and db columns

--- flask_intro/app.py
+++ flask_intro/app.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timezone
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+# /// is a relative path, //// is an abs path for sqlite
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False) #null false b/c don't want this blank
+    date_created = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
 
 @app.route('/')
 def index():
